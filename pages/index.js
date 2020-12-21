f38a89ff17bb9f6074f8de87eccd7acab5e86f0a
Nicholas Apolinario <nicholas.apolinario@outlook.com>
2020-12-21 18:42:47 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
 import {useState} from 'react';
 
 function Home() {
-   return <div>
-       <h1>Home 3^C</h1>
-       <Contador/>
-   </div>
+   return  <h1>Home 3^C</h1>
+      
 }
 export default Home
-function Contador(){
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-    return(
-        <div> 
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
